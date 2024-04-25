9dafd067ddf22cf274e5eb8115541b77a798db1e
Dinuka Navaratna <dinuka.navaratna@gmail.com>
2024-04-25 22:05:13 +0800

    flood info endpoints

--- src/routes.py
+++ src/routes.py
@@ -1,2 +1,14 @@
+from .users import Register, Login
+from .flood import SafeLocations, FloodHistory, FloodSeverity
+from .discussion import Discussion
+from .prediction import Predict
+
 def initialize_routes(api):
-    pass
+    api.add_resource(Register, "/register")
+    api.add_resource(Login, "/login")
+    api.add_resource(SafeLocations, "/safelocations")
+    api.add_resource(FloodHistory, "/floodhistory")
+    api.add_resource(FloodSeverity, "/severity")
+    api.add_resource(Discussion, "/discussion")
+    api.add_resource(Predict, "/predictions")
+    
